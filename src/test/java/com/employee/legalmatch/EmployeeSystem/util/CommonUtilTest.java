2b0c91a7cace23365f285d5272f1e60dd82aab89
John Marvie Biglang-awa <johnmarviebiglangawa@gmail.com>
2024-08-13 18:27:09 +0800

    feat: unit test for CommonUtil

---
+++ src/test/java/com/employee/legalmatch/EmployeeSystem/util/CommonUtilTest.java
@@ -0,0 +1,42 @@
+package com.employee.legalmatch.EmployeeSystem.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+public class CommonUtilTest {
+
+    @Test
+    void testStringNotNullOrEmpty() {
+        String given = "test";
+
+        boolean result = CommonUtil.isNotNullOrEmpty(given);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testStringNull() {
+        String given = null;
+
+        boolean result = CommonUtil.isNotNullOrEmpty(given);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testStringEmpty() {
+        String given = "";
+
+        boolean result = CommonUtil.isNotNullOrEmpty(given);
+
+        assertFalse(result);
+    }
+
+}
