cdce166d3331105c2dbe808418738dc8adea6f87
John Marvie Biglang-awa <johnmarviebiglangawa@gmail.com>
2024-08-15 23:40:25 +0800

    chore: added role_user to admin

--- src/main/resources/migration/data/V6__USER.sql
+++ src/main/resources/migration/data/V6__USER.sql
@@ -5,4 +5,4 @@ VALUES
 INSERT INTO User
     (Username, Hash, Roles)
 VALUES
-    ('admin', '$2a$10$8z1hFDo4MJJ9lpUQWbrBve0aieXN9nlJ0Tu8kOEhvVgKdCULn4PZ6', 'ROLE_ADMIN');
+    ('admin', '$2a$10$8z1hFDo4MJJ9lpUQWbrBve0aieXN9nlJ0Tu8kOEhvVgKdCULn4PZ6', 'ROLE_USER,ROLE_ADMIN');
